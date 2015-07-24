ad8c9f9eb629a65a998a5542757d2a6026ce023c
Tracy Chan <torayci@gmail.com>
2015-07-23 22:57:20 -0700

    finished blog comments feature

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723183924) do
+ActiveRecord::Schema.define(version: 20150724043935) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
